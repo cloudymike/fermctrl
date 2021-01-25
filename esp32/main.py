c7d8cb0d0f81c173a309f02d516e0edb19a11c3f
cloudymike <mikael@cloudyforecast.com>
2021-01-24 19:52:54 -0800

    wip

--- esp32/main.py
+++ esp32/main.py
@@ -8,6 +8,7 @@ import wlan
 import tempreader
 import awsiotconfig
 from mqtt_reader_aws import MQTTReaderAWS
+import relay
 
 
 class mainloop:
@@ -22,6 +23,9 @@ class mainloop:
             #Use old time
 
         self.unit='F'
+        self.hysterisis=0.5
+        self.temp=0.0
+        self.target = 0.0
         self.tempDevice = tempreader.tempreader(self.unit)
 
         self.m = MQTTReaderAWS(
@@ -33,6 +37,28 @@ class mainloop:
             awsiotconfig.CERT_FILE)
 
         self.txt = textout.textout()
+
+    def thermostat(self):
+        self.get_target()
+        self.get_temp()
+        print("Thermostat, temp:{} target:{}".format(self.temp, self.target))
+        if self.temp + self.hysterisis > self.target:
+            relay.COLD.on()
+        if self.temp - self.hysterisis < self.target:
+            relay.HOT.on()
+
+    def get_target(self):
+        targetstring = self.m.last_msg()
+        print(targetstring)
+        try:
+            self.target = float(targetstring)
+        except:
+            print("ERROR: No valid target")
+        return(self.target)
+
+    def get_temp(self):
+        self.temp = self.tempDevice.get_temp()
+        return(self.temp)
 
     def run(self):
         old_second = 99
@@ -47,14 +73,14 @@ class mainloop:
             # Cycle over x time
             if second != old_second:
                 old_second = second
-                temp = self.tempDevice.get_temp()
+                self.thermostat()
                 time_str = "UTC: {4:02d}:{5:02d}:{6:02d}".format(*current_time)
                 self.txt.clear()
                 self.txt.centerline(time_str,3)
 
-                self.txt.centerline("Temp: {:.1f}{}".format(temp,self.unit),4)
+                self.txt.centerline("Temp: {:.1f}{}".format(self.temp,self.unit),4)
 
-                self.txt.centerline("Target: {}".format(self.m.last_msg()),5)
+                self.txt.centerline("Target: {}".format(self.target),5)
                 self.txt.show()
 
 
