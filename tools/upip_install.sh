985a7f62d70b8f53a84ca83dacdcccac6e6ec1a2
cloudymike <mikael@cloudyforecast.com>
2021-05-24 21:59:10 -0700

    Remove unused files

--- tools/upip_install.sh
+++ tools/upip_install.sh
@@ -9,8 +9,9 @@ PORT='/dev/ttyUSB0'
 PUSHCMD="ampy --port $PORT put "
 CURDIR=$(pwd)
 TOPDIR=${CURDIR%/*}
+UPYEX=${TOPDIR}/micropythonexamples/DEVKITv1
 
 echo "Loading packages"
 $PUSHCMD ${WLAN_CONFIG_PATH}
-$PUSHCMD ../esp32/wlan.py
+PUSHCMD ${UPYEX}/wlan/wlan.py
 timeout 20  ampy --port /dev/ttyUSB0 run upip_install.py
