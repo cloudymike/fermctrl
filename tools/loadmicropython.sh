6e77a3d0658f431b64f1f507f0b3fa3a41c00e1c
cloudymike <mikael@cloudyforecast.com>
2022-07-17 19:30:53 -0700

    V1.18

--- tools/loadmicropython.sh
+++ tools/loadmicropython.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #PACKAGE=esp32-idf3-20200902-v1.13.bin
-PACKAGE=esp32-idf3-20190529-v1.11.bin
+
+# Last working
+#PACKAGE=esp32-idf3-20190529-v1.11.bin
+
+# Latest tested
+PACKAGE=esp32-20220117-v1.18.bin
+
 if [ ! -f $PACKAGE ]
 then
 	wget https://micropython.org/resources/firmware/$PACKAGE
