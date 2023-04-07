c0171b0e66cb958cf3095fc393aed05b0736481a
cloudymike <mikael@cloudyforecast.com>
2023-04-06 21:52:22 -0700

    initial

---
+++ prometheus/RUN.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PROMETHEUSVERSION=2.37.6
+PROMETHEUSDIR=prometheus-${PROMETHEUSVERSION}.linux-amd64
+if [ -d "${PROMETHEUSDIR}" ]
+then
+	echo prometheus is installed
+else
+	echo Installing prometheus version ${PROMETHEUSVERSION}
+    wget https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUSVERSION}/${PROMETHEUSDIR}.tar.gz
+    tar xvzf${PROMETHEUSDIR}
+fi
+
+
+${PROMETHEUSDIR}/prometheus --config.file=./prometheus.yml
+
