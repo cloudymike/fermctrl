c0d3fe73b1e0283e50ce659873f23029cb975f8e
cloudymike <mikael@cloudyforecast.com>
2023-04-08 23:05:49 -0700

    woring

--- prometheus/buildPrometheus.py
+++ prometheus/buildPrometheus.py
@@ -6,28 +6,22 @@ import sys
 fr = open('prometheus_example.yml','r')
 pconfig = yaml.safe_load(fr)
 
-#grafana = []
-#gauth={}
-#gauth['username']=config.grafana_username
-#gauth['password']=config.grafana_password
-#grafana.append({'basic_auth':gauth})
-#grafana.append({'url':config.grafana_url})
-#pconfig['remote_write'] = grafana
-
-pconfig['remote_write'][0]= {'url': config.grafana_url}
-pconfig['remote_write'].append({'basic_auth':{'username': config.grafana_password, 'password': config.grafana_password}})
-
-#pconfig['rule_files']={}
-
-sc = []
-
-sc.append({'job_name':config.device_name})
-sc.append({'static_configs':[{'targets': ['localhost:8081']}]})
-pconfig['scrape_configs']=sc
+pconfig['remote_write'] = [
+    {
+    	'url': config.grafana_url,
+    	'basic_auth':
+         {
+         	'username': config.grafana_username,
+         	'password': config.grafana_password
+         }
+    }
+]
+pconfig['scrape_configs'] = [ {
+    'job_name': config.device_name, 
+    'static_configs': [{'targets': ['localhost:8081']} ]
+    } ]
 
 
 fw = open('prometheus.yml', 'w')
 yaml.dump(pconfig,fw)
-
-print(yaml.dump(pconfig))
 
