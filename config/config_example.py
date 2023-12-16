edc354b7d249b2bd9140fcde649c7ba12b711753
cloudymike <mikael@cloudyforecast.com>
2023-12-15 22:08:51 -0800

    Update example (#129)

--- config/config_example.py
+++ config/config_example.py
@@ -2,9 +2,10 @@
 use_google = False
 
 project = "tempctrlproj"
-device_name  = "lagercooler"
+device_name  = "testcooler"
+device_list = [device_name, "lagercooler"]
 device_data = "esp32data"
 app_data = "settings"
 device_topic = "{}/{}/{}".format(project,device_name,device_data)
 app_topic = "{}/{}/{}".format(project,device_name,app_data)
-hostname = '192.168.62.241'
+hostname = '192.168.123.123'
